10b1ae228b2252916599d47b5919b64fd0d3b4f7
jkellerman <89940123+jkellerman@users.noreply.github.com>
2021-10-07 16:25:55 +0100

    create a print method to class and print the current field

--- main.js
+++ main.js
@@ -6,5 +6,26 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-  
+  constructor(field) {
+      this._field = field;
+  }
+
+  get field(){
+      return this._field;
+  }
+
+  //print the field to the terminal in a 2D plane
+  print() {
+    return this._field.map(row => row.join('')).join('\n')
+  }
 }
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+console.log(myField.print());
+
+
