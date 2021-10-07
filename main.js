2df2843a63ebf529c23cffd85205e9fb3cff6a8a
jkellerman <89940123+jkellerman@users.noreply.github.com>
2021-10-07 17:13:44 +0100

    add prompt for user input

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const pathCharacter = '*';
 class Field {
   constructor(field) {
       this._field = field;
+      this.y = 0;
+      this.x = 0;
   }
 
   get field(){
@@ -18,6 +20,32 @@ class Field {
   print() {
     return this._field.map(row => row.join('')).join('\n')
   }
+
+  //prompt user input and move the player cursor
+  
+  prompt(){
+    let direction = prompt('Which Way? (u for Up, d for Down, l for Left, r for Right)');
+    switch(direction.toLowerCase()) {
+      case 'u':
+        this.y -= 1;
+        break;
+
+      case 'd':
+        this.y += 1;
+        break;
+
+      case 'l':
+        this.x -= 1;
+        break;
+
+      case 'r':
+        this.x += 1;
+        break;
+
+      default:
+        break;
+    }
+  }
 }
 
 const myField = new Field([
@@ -27,5 +55,5 @@ const myField = new Field([
   ]);
 
 console.log(myField.print());
-
+myField.prompt();
 
